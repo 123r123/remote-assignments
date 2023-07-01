8e8762748617ddf1682b523d82c2f21bbcd03f8a
Le Sheng Chen <raymand0109@gmail.com>
2023-07-02 01:14:52 +0800

    0701

---
+++ Week-1/Assignment-3/callback_func.js
@@ -0,0 +1,12 @@
+function delayedResult(n1, n2, delayTime, callback) {
+    // your code here
+    setTimeout(function () {
+        callback(n1+n2);
+    }, delayTime);
+}
+delayedResult(4, 5, 3000, function (result) {
+    console.log(result);
+}); // 9 (4+5) will be shown in the console after 3 seconds
+delayedResult(-5, 10, 2000, function (result) {
+    console.log(result);
+}); // 5 (-5+10) will be shown in the console after 2 seconds
